5ea005662dcc4d206d92f1eae09caf2f22ef5710
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-05-20 14:32:13 +0530

    Created new project

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by vinay Kumar ranjan on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
