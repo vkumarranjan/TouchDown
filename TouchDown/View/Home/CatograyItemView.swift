3dc59181b494a327e84d6d9e384e9202afd10c50
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-05-26 12:22:33 +0530

    Added catogryView

---
+++ TouchDown/View/Home/CatograyItemView.swift
@@ -0,0 +1,18 @@
+//
+//  CatograyItemView.swift
+//  TouchDown
+//
+//  Created by vinay Kumar ranjan on 24/05/24.
+//
+
+import SwiftUI
+
+struct CatograyItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatograyItemView()
+}
---
+++ TouchDown/View/Home/CatogryGridView.swift
@@ -0,0 +1,18 @@
+//
+//  CatogryGridView.swift
+//  TouchDown
+//
+//  Created by vinay Kumar ranjan on 24/05/24.
+//
+
+import SwiftUI
+
+struct CatogryGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatogryGridView()
+}
